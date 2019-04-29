705cac9d7fc9f1502331feadba7051b6ac430aa5
Marijan3062 <brailo.marijan@gmail.com>
2019-04-29 16:14:57 +0200

    First commit

---
+++ Javascript.js
@@ -0,0 +1,15 @@
+function SimpleAdding(num) { 
+
+    var answer = 0;
+  
+    // loop from 1 to num
+    for (var i = 1; i <= num; i++) {
+      answer = answer + i;
+    } 
+  
+    return answer;
+           
+  }
+  
+  SimpleAdding(4);                                                    
+  
